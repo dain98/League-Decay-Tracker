c386f2d9411301e77aa6a2bb7cf801361950bdc8
imdatwit <imd@wit.edu>
2025-06-30 18:53:49 -0400

    oops

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     port: 5173,
-    allowedHosts: import.meta.env.VITE_ALLOWED_HOSTS ? import.meta.env.VITE_ALLOWED_HOSTS.split(',') : ['localhost'],
+    allowedHosts: process.env.VITE_ALLOWED_HOSTS ? process.env.VITE_ALLOWED_HOSTS.split(',') : ['localhost'],
   },
   plugins: [react()],
 })
